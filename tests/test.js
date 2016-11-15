da01752d9a574795a4b91e02419071af828a1f0b
darthbatman <darthanakin44@gmail.com>
2016-11-15 18:25:41 -0500

    fixed shifted ranks due to missing song

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 var billboard = require("../billboard-top-100.js").getChart;
 
-billboard('hot-100', '2016-08-27', function(songs, err){
+billboard('hot-100', '1961-09-23', function(songs, err){
 	if (err) console.log(err);
 	console.log(songs);
 });

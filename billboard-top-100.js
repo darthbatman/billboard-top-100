4de64db61f3cb58655c16d5a10f0ac7fe034be89
XiaoMing Liu <xper119@live.com>
2017-10-01 09:48:32 -0500

    Update billboard-top-100.js
    patch

--- billboard-top-100.js
+++ billboard-top-100.js
@@ -3,14 +3,13 @@ var cheerio = require("cheerio");
 
 const baseUrl = "http://www.billboard.com/charts/";
 
+// list all data from requested chart
+
 var getChart = function(chart, date, cb){
 	var result;
 	if (typeof date === 'function'){
 		cb = date;
 		date = '';
-	} else if (typeof date === 'string') {
-		date = tunningDate(date);
-		result = {'chartTime': date};
 	}
 
 	var songs = [];
@@ -25,46 +24,47 @@ var getChart = function(chart, date, cb){
 
 			var $ = cheerio.load(html);
 
-			$('.chart-row__song').each(function(index, item){
-				var songName = $(this).text().replace(/\r?\n|\r/g, "").replace(/\s+/g, ' ');
+			$('.chart-data article').each(function(index, item){
+				var songName = $('.chart-row__song', this).text().replace(/\r?\n|\r/g, "").replace(/\s+/g, ' ');
 				while(songName[0] === ' ')
     				songName = songName.substr(1);
 				titles.push(songName);
 
-				$(item).closest('article').find('.chart-row__secondary > div').each(function(_, item) {
+				$(item).find('.chart-row__secondary > div').each(function(_, item) {
 					var positionInfo = {};
 					$(item).children('div').each(function(_, item) {
 						positionInfo[$('span:first-child', item).text()] = $('span:last-child', item).text()
 					});
 					positions.push(positionInfo);
 				});
-			});
+				
+				$('.chart-row__current-week', item).each(function(index){
+					ranks.push($(this).text().replace(/\r?\n|\r/g, "").replace(/\s+/g, ' '));
+				});
 
-			$('.chart-row__current-week').each(function(index){
-				ranks.push($(this).text().replace(/\r?\n|\r/g, "").replace(/\s+/g, ' '));
-			});
+				$('.chart-row__artist', item).each(function(index){
+					var artistName = $(this).text().replace(/\r?\n|\r/g, "").replace(/\s+/g, ' ');
+					while(artistName[0] === ' ')
+	    				artistName = artistName.substr(1);
+	    			if (artistName[artistName.length - 1] == ' '){
+	    				artistName = artistName.substring(0, artistName.length - 1);
+	    			}
+					artists.push(artistName);
+				});
+				
+				$('.chart-row__image', item).each(function(index){
+					var style = $(this).attr("style");
+					if(style){
+						var songCover= style.replace("background-image: url(http://","").replace(")","");
+					}else{
+						var data = $(this).attr("data-imagesrc");
+						if(data){
+							var songCover = data.replace("http://","");
+						}	
+					}
+					covers.push(songCover);
+				});
 
-			$('.chart-row__artist').each(function(index){
-				var artistName = $(this).text().replace(/\r?\n|\r/g, "").replace(/\s+/g, ' ');
-				while(artistName[0] === ' ')
-    				artistName = artistName.substr(1);
-    			if (artistName[artistName.length - 1] == ' '){
-    				artistName = artistName.substring(0, artistName.length - 1);
-    			}
-				artists.push(artistName);
-			});
-			
-			$('.chart-row__image').each(function(index){
-				var style = $(this).attr("style");
-				if(style){
-					var songCover= style.replace("background-image: url(http://","").replace(")","");
-				}else{
-					var data = $(this).attr("data-imagesrc");
-					if(data){
-						var songCover = data.replace("http://","");
-					}	
-				}
-				covers.push(songCover);
 			});
 
 			if (titles.length > 1){
@@ -95,57 +95,36 @@ var getChart = function(chart, date, cb){
 
 }
 
-/**
- * list the available chart page
- */
-var manifest = function(cb) {
+// list the available charts
+
+var listCharts = function(cb) {
 	request(baseUrl, function(error, response, html) {
-		var result = {};
+		var charts = {};
 		if (error) {
-			cb(result, error)
+			cb(charts, error)
 			return;
 		}
 		var $ = cheerio.load(html);
 		var prefixOfLink = '/charts/';
 
-		$('#main-wrapper :header').each(function(_, head) {
-			var links = new Set();
-			$(head).nextUntil(':header', ':has(a)').each(function(_, item) {
+		$('#main h3').each(function(_, head) {
+			var links = [];
+			$(head).nextUntil('h3', ':has(a):not(:header)').each(function(_, item) {
 				var address = $('a', item).attr('href') || '';
 				var startIndex = -1;
 				if ((startIndex = address.indexOf(prefixOfLink)) !== -1) {
-					links.add(address.substring(startIndex + prefixOfLink.length))
+					links.push(address.substring(startIndex + prefixOfLink.length));
 				}
 			});
-			result[$(head).text()] = [...links];
-			links.clear();
+			charts[$(head).text()] = links;
 		});
 		if (typeof cb === 'function') {
-			cb(result);
+			cb(charts);
 		}
 	});
 }
 
-/**
- * allow any date
- * if the date which given by user is not saturday,
- * it will be automatically adjusted to the saturday of the last week 
- */
-function tunningDate(date) {
-	if (/\d{4}-\d{2}-\d{2}/.test(date)) {
-		var day = new Date(date);
-		var dayOfWeek = day.getDay();
-		var endOfWeek = dayOfWeek === 6;
-		var timestampOfDay = day.getTime();
-		if (!endOfWeek) {
-			var saturday = new Date(timestampOfDay - (dayOfWeek + 1) * 24 * 60 * 60 * 1000);
-			return saturday.toISOString().substring(0, 10);
-		}
-	}
-	return date;
-}
-
 module.exports = {
 	getChart,
-	manifest
+	listCharts
 }
